4de99d134944ec3beefbb6da8dd7690bb9b91ffb
Joe Cooler <joe_cooler@msn.com>
2020-03-30 22:24:36 -0500

    code done

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,6 +4,7 @@ var apiKey = "&api-key=oLmoudeBzeuVglDNxSArduM8cpGpAa6G";
 $("button").on("click",function () {
 
     var queryText = $("#searchBox").val();
+    var results = $("#number-of-records").val();
 
     $.ajax({
         url: queryURL + queryText + apiKey,
@@ -11,7 +12,11 @@ $("button").on("click",function () {
     
     }) .then(function(response) {
         console.log(response);
-        $("#results").append("<div>" + response.response.docs[0].headline.main + "</div>");
+        $("#results").empty();
+        for (i = 0; i < results; i++)
+        {
+        $("#results").append("<div><a href='" + response.response.docs[i].web_url + "'>" + response.response.docs[i].headline.main + "</a></div>");
+        }
         
 
 })
